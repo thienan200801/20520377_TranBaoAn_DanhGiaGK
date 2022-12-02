bfb4241663ea72ecf9a805c8c65628754dce5c56
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2022-12-02 13:58:21 +0700

    2

--- 20520377_TranBaoAn_DanhGiaGK/Program.cs
+++ 20520377_TranBaoAn_DanhGiaGK/Program.cs
@@ -34,35 +34,6 @@ namespace _20520377_TranBaoAn_DanhGiaGK
             f.Size = new Size(2000, 1000);
             f.Controls.Add(MatHangButton);
 
-            Label msLabel = new Label();
-            msLabel.Text = "Ma so";
-            msLabel.Location = new Point(600, 100);
-            f.Controls.Add(msLabel);
-
-            TextBox ms = new TextBox();
-            ms.Location = new Point(700, 100);
-            f.Controls.Add(ms);
-            //
-            Label tenLabel = new Label();
-            tenLabel.Text = "Ten";
-            tenLabel.Location = new Point(600, 200);
-            f.Controls.Add(tenLabel);
-
-            TextBox ten = new TextBox();
-            ten.Location = new Point(700, 200);
-            f.Controls.Add(ms);
-
-            //
-            Label giaLabel = new Label();
-            giaLabel.Text = "Ma so";
-            giaLabel.Location = new Point(600, 300);
-            f.Controls.Add(giaLabel);
-
-            TextBox gia = new TextBox();
-            gia.Location = new Point(700, 300);
-            f.Controls.Add(gia);
-            //
-
             Button load = new Button();
             load.Text = "Load";
             load.Location = new Point(500, 500);
@@ -78,36 +49,89 @@ namespace _20520377_TranBaoAn_DanhGiaGK
 
         private static void loadFile(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                string filepath = ofd.FileName;
-                Console.WriteLine(filepath);
+            string text = System.IO.File.ReadAllText(@"C:\Users\ThienAn\OneDrive - gm.uit.edu.vn\Documents");
 
-                //TreeNode tNode;
-                //tNode = treeView1.Nodes.Add("Websites");
+            Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //if (ofd.ShowDialog() == DialogResult.OK)
+            //{
+            //    string filepath = ofd.FileName;
+            //    Console.WriteLine(filepath);
 
-                //treeView1.Nodes[0].Nodes.Add("Net-informations.com");
-                //treeView1.Nodes[0].Nodes[0].Nodes.Add("CLR");
+            //    TreeNode tNode;
+            //    tNode = treeView1.Nodes.Add("Websites");
 
-                //treeView1.Nodes[0].Nodes.Add("Vb.net-informations.com");
-                //treeView1.Nodes[0].Nodes[1].Nodes.Add("String Tutorial");
-                //treeView1.Nodes[0].Nodes[1].Nodes.Add("Excel Tutorial");
+            //    treeView1.Nodes[0].Nodes.Add("Net-informations.com");
+            //    treeView1.Nodes[0].Nodes[0].Nodes.Add("CLR");
 
-                //treeView1.Nodes[0].Nodes.Add("Csharp.net-informations.com");
-                //treeView1.Nodes[0].Nodes[2].Nodes.Add("ADO.NET");
-                //treeView1.Nodes[0].Nodes[2].Nodes[0].Nodes.Add("Dataset");
-            }    
+            //    treeView1.Nodes[0].Nodes.Add("Vb.net-informations.com");
+            //    treeView1.Nodes[0].Nodes[1].Nodes.Add("String Tutorial");
+            //    treeView1.Nodes[0].Nodes[1].Nodes.Add("Excel Tutorial");
+
+            //    treeView1.Nodes[0].Nodes.Add("Csharp.net-informations.com");
+            //    treeView1.Nodes[0].Nodes[2].Nodes.Add("ADO.NET");
+            //    treeView1.Nodes[0].Nodes[2].Nodes[0].Nodes.Add("Dataset");
+
+            //    f.Controls.Add(treeView1);
+            //}    
         }
 
         private static void ThemMatHang(object sender, EventArgs e)
         {
-            
+            Form f1 = new Form();
+            f1.Size = new Size(700, 700);
+            Label msLabel = new Label();
+            msLabel.Text = "Ma so";
+            msLabel.Location = new Point(100, 100);
+            f1.Controls.Add(msLabel);
+
+            TextBox ms = new TextBox();
+            ms.Location = new Point(300, 100);
+            f1.Controls.Add(ms);
+            //
+            Label tenLabel = new Label();
+            tenLabel.Text = "Ten";
+            tenLabel.Location = new Point(100, 200);
+            f1.Controls.Add(tenLabel);
+
+            TextBox ten = new TextBox();
+            ten.Location = new Point(300, 200);
+            f1.Controls.Add(ten);
+            //
+            Label giaLabel = new Label();
+            giaLabel.Text = "Gia";
+            giaLabel.Location = new Point(100, 300);
+            f1.Controls.Add(giaLabel);
+
+            TextBox gia = new TextBox();
+            gia.Location = new Point(300, 300);
+            f1.Controls.Add(gia);
+
+            f1.ShowDialog();
         }
 
         private static void ThemNganhHang(object sender, EventArgs e)
         {
-           
+            Form f2 = new Form();
+            f2.Size = new Size(700, 700);
+            Label msLabel = new Label();
+            msLabel.Text = "Ma so";
+            msLabel.Location = new Point(100, 100);
+            f2.Controls.Add(msLabel);
+
+            TextBox ms = new TextBox();
+            ms.Location = new Point(300, 100);
+            f2.Controls.Add(ms);
+            //
+            Label tenLabel = new Label();
+            tenLabel.Text = "Ten";
+            tenLabel.Location = new Point(100, 200);
+            f2.Controls.Add(tenLabel);
+
+            TextBox ten = new TextBox();
+            ten.Location = new Point(300, 200);
+            f2.Controls.Add(ten);
+            f2.ShowDialog();
         }
 
 
